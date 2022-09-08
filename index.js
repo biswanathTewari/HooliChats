206ad2dd0c29d3826e4bb2c4931390beb995b3dc
Biswanath Tewari <biswanath@geekyants.com>
2022-09-08 15:39:07 +0530

    ✨ feat: mockBee integration

--- index.js
+++ index.js
@@ -6,4 +6,9 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 
+import {makeServer} from './src/server';
+
+// Call make Server
+makeServer();
+
 AppRegistry.registerComponent(appName, () => App);
